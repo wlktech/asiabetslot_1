24436d329a712ee80c717bf00d51bcb612961390
Wai Linn Kyaw <108474411+wlktech@users.noreply.github.com>
2024-01-06 23:03:40 +0630

    a

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -98,56 +98,16 @@ export default function Login() {
         <div className="row align-items-center justify-content-lg-between">
             <div className="col-12 col-md-6 my-auto">
             <div className="copyright text-center text-sm text-white text-lg-start">
-                © , made with <i className="fa fa-heart" aria-hidden="true" /> by
+                ©Copyright 2024 , made with <i className="fa fa-heart" aria-hidden="true" /> by
                 <a
-                href="https://www.creative-tim.com"
-                className="font-weight-bold text-white"
+                href="https://delightmyanmar.pro"
+                className="font-weight-bold text-white mx-2"
                 target="_blank"
                 >
-                Creative Tim
+                 Delight Myanmar 
                 </a>
                 for a better web.
             </div>
-            </div>
-            <div className="col-12 col-md-6">
-            <ul className="nav nav-footer justify-content-center justify-content-lg-end">
-                <li className="nav-item">
-                <a
-                    href="https://www.creative-tim.com"
-                    className="nav-link text-white"
-                    target="_blank"
-                >
-                    Creative Tim
-                </a>
-                </li>
-                <li className="nav-item">
-                <a
-                    href="https://www.creative-tim.com/presentation"
-                    className="nav-link text-white"
-                    target="_blank"
-                >
-                    About Us
-                </a>
-                </li>
-                <li className="nav-item">
-                <a
-                    href="https://www.creative-tim.com/blog"
-                    className="nav-link text-white"
-                    target="_blank"
-                >
-                    Blog
-                </a>
-                </li>
-                <li className="nav-item">
-                <a
-                    href="https://www.creative-tim.com/license"
-                    className="nav-link pe-0 text-white"
-                    target="_blank"
-                >
-                    License
-                </a>
-                </li>
-            </ul>
             </div>
         </div>
         </div>
--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -115,61 +115,21 @@ export default function Register() {
         </div>
         <footer className="footer position-absolute bottom-2 py-2 w-100">
             <div className="container">
-            <div className="row align-items-center justify-content-lg-between">
-                <div className="col-12 col-md-6 my-auto">
-                <div className="copyright text-center text-sm text-white text-lg-start">
-                    © , made with <i className="fa fa-heart" aria-hidden="true" /> by
-                    <a
-                    href="https://www.creative-tim.com"
-                    className="font-weight-bold text-white"
-                    target="_blank"
-                    >
-                    Creative Tim
-                    </a>
-                    for a better web.
+                <div className="row align-items-center justify-content-lg-between">
+                    <div className="col-12 col-md-6 my-auto">
+                        <div className="copyright text-center text-sm text-white text-lg-start">
+                            ©Copyright 2024 , made with <i className="fa fa-heart" aria-hidden="true" /> by
+                            <a
+                            href="https://delightmyanmar.pro"
+                            className="font-weight-bold text-white mx-2"
+                            target="_blank"
+                            >
+                            Delight Myanmar 
+                            </a>
+                            for a better web.
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div className="col-12 col-md-6">
-                <ul className="nav nav-footer justify-content-center justify-content-lg-end">
-                    <li className="nav-item">
-                    <a
-                        href="https://www.creative-tim.com"
-                        className="nav-link text-white"
-                        target="_blank"
-                    >
-                        Creative Tim
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a
-                        href="https://www.creative-tim.com/presentation"
-                        className="nav-link text-white"
-                        target="_blank"
-                    >
-                        About Us
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a
-                        href="https://www.creative-tim.com/blog"
-                        className="nav-link text-white"
-                        target="_blank"
-                    >
-                        Blog
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a
-                        href="https://www.creative-tim.com/license"
-                        className="nav-link pe-0 text-white"
-                        target="_blank"
-                    >
-                        License
-                    </a>
-                    </li>
-                </ul>
-                </div>
-            </div>
             </div>
         </footer>
         </div>
